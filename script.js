06d1b6bbe734c44d339a8ab9d5ab0633b3fed514
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-07-17 18:06:16 +0500

    Added returns for every function

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let sum = 0;
 function add(a, b) {
-    sum = a + b;
+    return sum = a + b;
 }
 
 function subtract(a, b) {
-    sum = a - b;
+    return sum = a - b;
 }
 
 function multiply(a, b) {
-    sum = a * b;
+    return sum = a * b;
 }
 
 function divide(a, b) {
-    sum = a / b;
+    return sum = a / b;
 }
