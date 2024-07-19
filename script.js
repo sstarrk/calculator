73173529e46436a8832afc02374244ea64087283
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-07-19 21:20:57 +0500

    Added displaying pressed numbers on screen

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const display = document.querySelector("#screen");
+display.textContent = 0;
+
+const nums = document.querySelectorAll(".numbers");
+nums.forEach((num) => {
+    num.addEventListener("click", () => {
+        if(display.textContent == "0") {
+            display.textContent = num.textContent;
+        } else {
+            display.textContent = display.textContent.toString() + num.textContent.toString();
+        }
+        a = display.textContent;
+    })
+})
+
 let sum = 0;
 let a;
 let b;
@@ -30,3 +45,4 @@ function operate(a, operator, b){
         divide(a, b);
     }
 }
+
